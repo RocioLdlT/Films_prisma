a0d3a6bb1d1b12a2a2b5ce645b9b62bb137fc90f
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-23 13:58:46 +0100

    Add service auth.repo.user

---
+++ src/services/auth.ts
@@ -0,0 +1,18 @@
+import { env } from '../config/env.ts';
+import debug from 'debug';
+import { hash, compare } from 'bcryptjs';
+
+const log = debug( `${env.PROJECT_NAME}:service.auth`);
+log('Loading auth service...');
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class AuthService {
+
+    static hash (password: string): Promise<string> {
+        return hash(password, 10)
+
+    }
+    static compare(password: string, hash: string): Promise<boolean>{
+        return compare(password, hash)
+    }
+}
