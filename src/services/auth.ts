23b185d55ed6c98ff2fc2000d9d1b85667befc67
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-27 13:29:59 +0100

    Update  auth.ts with  two new methods to use jwt_secret

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,18 +1,36 @@
 import { env } from '../config/env.ts';
 import debug from 'debug';
 import { hash, compare } from 'bcryptjs';
+import type { TokenPayload } from '../types/login.ts';
+import jwt  from 'jsonwebtoken';
 
 const log = debug( `${env.PROJECT_NAME}:service.auth`);
 log('Loading auth service...');
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class AuthService {
-
+    static saltRound = 12 //Mejor guardar el saltRound en .env para no dar este detalle de nº de vueltas.
     static hash (password: string): Promise<string> {
-        return hash(password, 10)
+        return hash(password, this.saltRound)
 
     }
     static compare(password: string, hash: string): Promise<boolean>{
         return compare(password, hash)
+    }
+     static generateToken(payload: TokenPayload): string {
+        return jwt.sign(
+            payload,
+            env.JWT_SECRET,
+            //{ expiresIn: '1h' }
+        );
+    }
+
+    static verifyToken(token: string): TokenPayload | null {
+        try {
+            return jwt.verify(token, env.JWT_SECRET) as TokenPayload;
+        } catch (error) {
+            log('Invalid token:', error);
+            return null;
+        }
     }
 }
