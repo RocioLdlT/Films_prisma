76e6e88932ee3bac114828631908140de94a763a
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-28 13:10:55 +0100

    Add jwt_ secret  in .env to correct auth.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,26 +3,27 @@ import { ZodError } from 'zod';
 import debug from 'debug';
 
 export const EnvSchema = z.object({
-    PORT: z.coerce.number(),
-    NODE_ENV: z.enum(['dev', 'prod', 'test']),
-    DEBUG: z.string().optional(),
-    PROJECT_NAME: z.string(),
-    PGUSER: z.string(),
-    PGPASSWORD: z.string(),
-    PGHOST: z.string(),
-    PGPORT: z.coerce.number(),
-    PGDATABASE: z.string(),
-    // DB_URL: z.string().optional
+  PORT: z.coerce.number(),
+  NODE_ENV: z.enum(['dev', 'prod', 'test']),
+  PROJECT_NAME: z.string(),
+  DEBUG: z.string().optional(),
+  JWT_SECRET: z.string().min(32), // Asegura que la clave sea al menos de 32 caracteres
+  PGUSER: z.string(),
+  PGPASSWORD: z.string(),
+  PGHOST: z.string(),
+  PGPORT: z.coerce.number(),
+  PGDATABASE: z.string(),
+  // DB_URL: z.string().optional
 });
 
 export type Env = z.infer<typeof EnvSchema>;
 export let env: Env;
 
 try {
-    env = EnvSchema.parse(process.env);
-    const log = debug(`${env.PROJECT_NAME}:env`);
-    log('Loading environment variables.');
+  env = EnvSchema.parse(process.env);
+  const log = debug(`${env.PROJECT_NAME}:env`);
+  log('Loading environment variables.');
 } catch (error) {
-    console.log(error as ZodError);
-    process.exit(1);
+  console.log(error as ZodError);
+  process.exit(1);
 }
