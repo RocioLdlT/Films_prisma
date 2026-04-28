f848182a761a757139e46ad71032037374db2b2c
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-28 13:57:08 +0100

    Create const startServer and catch error

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,29 @@
-import { env } from "./config/env.ts";
+import { env } from './config/env.ts';
 import debug from 'debug';
-import { createServer } from "node:http";
-import { createApp } from "./app.ts";
-import { connectDB } from "./config/db.ts";
+import { createServer } from 'node:http';
+import { createApp } from './app.ts';
+import { connectDB } from './config/db.ts';
 
 const log = debug(`${env.PROJECT_NAME}:index`);
-log("Starting API server...");
-
-const prisma = await connectDB();
-const port = env.PORT || 3000;
-const app = createApp(prisma);
-
-const server = createServer(app);
-log('Server created');
+log('Starting API server...');
 
 const listenManager = () => {
-    const addr = server.address();
-    if (addr === null) return;
-    let bind;
-    if (typeof addr === 'string') {
-        bind = 'pipe ' + addr;
-    } else {
-        bind =
-            addr.address === '::'
-                ? `http://localhost:${addr?.port}`
-                : `${addr.address}:${addr?.port}`;
-    }
-    if (env.NODE_ENV !== 'dev') {
-        console.log(`Server listening on ${bind}`);
-    } else {
-        log(`Servidor escuchando en ${bind}`);
-    }
+  const addr = server.address();
+  if (addr === null) return;
+  let bind;
+  if (typeof addr === 'string') {
+    bind = 'pipe ' + addr;
+  } else {
+    bind =
+      addr.address === '::'
+        ? `http://localhost:${addr?.port}`
+        : `${addr.address}:${addr?.port}`;
+  }
+  if (env.NODE_ENV !== 'dev') {
+    console.log(`Server listening on ${bind}`);
+  } else {
+    log(`Servidor escuchando en ${bind}`);
+  }
 };
 
 // const errorManager = (error: HttpError, response: ServerResponse) => {
@@ -48,26 +41,34 @@ const listenManager = () => {
 //     response.end(errorInfo);
 // };
 
+// Lo incluimos dentro de startServer:
+
+// const prisma = await connectDB();
+// const port = env.PORT || 3000;
+// const app = createApp(prisma);
+
+// const server = createServer(app);
+// log('Server created');
+
+// server.on('listening', listenManager);
+// server.listen(port);
 
 const startServer = async () => {
-    log('Starting API server...');
-    const prisma = await connectDB();
-    const app = createApp(prisma);
-    const port = env.PORT || 3000;
-    const server = createServer(app);
-    log('Server created');
-    server.listen(port);
-    server.on('listening', listenManager);
-    // server.on('error', errorManager);
-    return server
+  log('Starting API server...');
+  const prisma = await connectDB();
+  const app = createApp(prisma);
+  const port = env.PORT || 3000;
+  const server = createServer(app);
+  log('Server created');
+  server.listen(port);
+  server.on('listening', listenManager);
+  // server.on('error', errorManager);
+  return server;
 };
 
 const server = await startServer().catch((error) => {
-    log('Error starting server:', error);
-    process.exit(1);
+  log('Error starting server:', error);
+  process.exit(1);
 });
 
-// server.on('listening', listenManager);
-// // server.on('error', errorManager);
-// server.listen(port);
 
