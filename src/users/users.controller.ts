8e446fc793bb707cfe731163928ebc1fccc0cc2f
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-29 12:35:25 +0100

    Add user controller and router

---
+++ src/users/users.controller.ts
@@ -0,0 +1,222 @@
+import { env } from '../config/env.ts';
+import debug from 'debug';
+import type { UsersRepo } from './users.repo.ts';
+import type { NextFunction, Request, Response } from 'express';
+import type {
+  LoginUserData,
+  ProfileDTO,
+  RegisterUserData,
+  User,
+  UserUpdateDTO,
+} from './user.schemas.zod.ts';
+import { HttpError } from '../errors/http-error.ts';
+import type { LoginResult } from '../types/login.ts';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+
+const log = debug(`${env.PROJECT_NAME}:controller:users`);
+log('Loading users controller...');
+
+export class UsersController {
+  private repo: UsersRepo;
+  constructor(repo: UsersRepo) {
+    this.repo = repo;
+  }
+
+  async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      log('Registering new user...');
+      const userData: RegisterUserData = req.body;
+      // Validated previously with zod middleware
+      const user: User = await this.repo.register(userData);
+      return res.status(201).json(user);
+    } catch (error) {
+      log('Error registering user: %O', error);
+      const finalError = new HttpError(
+        500,
+        'Internal Server Error',
+        'Failed to register user',
+        {
+          cause: error,
+        },
+      );
+      return next(finalError);
+    }
+  }
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      log('Logging in user...');
+      const loginData: LoginUserData = req.body;
+      // Validated previously with zod middleware
+      const loginResult: LoginResult = await this.repo.login(loginData);
+      return res.json(loginResult);
+    } catch (error) {
+      log('Error logging in user: %O', error);
+      if (error instanceof PrismaClientKnownRequestError) {
+        const finalError = new HttpError(
+          401,
+          'Unauthorized',
+          'Invalid email or password',
+          {
+            cause: error,
+          },
+        );
+        return next(finalError);
+      }
+      const finalError = new HttpError(
+        500,
+        'Internal Server Error',
+        'Failed to login user',
+        {
+          cause: error,
+        },
+      );
+      return next(finalError);
+    }
+  }
+
+  async getAllUsers(req: Request, res: Response, next: NextFunction) {
+    try {
+      log('Getting all users...');
+      const users: User[] = await this.repo.getAllUsers();
+      return res.json(users);
+    } catch (error) {
+      log('Error getting all users: %O', error);
+      const finalError = new HttpError(
+        500,
+        'Internal Server Error',
+        'Failed to get users',
+        {
+          cause: error,
+        },
+      );
+      return next(finalError);
+    }
+  }
+
+  async getUserById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = Number(req.params.id);
+      // Validated previously with zod middleware
+      log('Get User: %O', id);
+      const user: User = await this.repo.getUserById(id);
+      return res.json(user);
+    } catch (error) {
+      log('Error getting user by id: %O', error);
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const finalError = new HttpError(404, 'Not Found', 'User not found', {
+          cause: error,
+        });
+        return next(finalError);
+      }
+
+      const finalError = new HttpError(
+        500,
+        'Internal Server Error',
+        'Failed to get user',
+        {
+          cause: error,
+        },
+      );
+      return next(finalError);
+    }
+  }
+
+  async updateUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = Number(req.params.id);
+      // Validated previously with zod middleware
+      log('Updating user with ID: %O', id);
+      const userData: UserUpdateDTO = req.body;
+      // Validated previously with zod middleware
+      const user: User = await this.repo.updateUser(id, userData);
+      return res.json(user);
+    } catch (error) {
+      log('Error updating user: %O', error);
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const finalError = new HttpError(404, 'Not Found', 'User not found', {
+          cause: error,
+        });
+        return next(finalError);
+      }
+
+      const finalError = new HttpError(
+        500,
+        'Internal Server Error',
+        'Failed to update user',
+        {
+          cause: error,
+        },
+      );
+      return next(finalError);
+    }
+  }
+
+  async updateProfileUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      log('Updating user profile...');
+      const id = Number(req.params.id);
+      // Validate previously with zod middleware
+      const profileData: Partial<ProfileDTO> = req.body; // Validate this data in a real application
+      const user: User = await this.repo.updateUserProfile(id, profileData);
+      return res.json(user);
+    } catch (error) {
+      log('Error updating user profile: %O', error);
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const finalError = new HttpError(404, 'Not Found', 'User not found', {
+          cause: error,
+        });
+        return next(finalError);
+      }
+
+      const finalError = new HttpError(
+        500,
+        'Internal Server Error',
+        'Failed to update user profile',
+        {
+          cause: error,
+        },
+      );
+      return next(finalError);
+    }
+  }
+
+  async deleteUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = Number(req.params.id);
+      // Validated previously with zod middleware
+      log('Deleting user with ID: %O', id);
+      await this.repo.deleteUser(id);
+      return res.status(204).end();
+    } catch (error) {
+      log('Error deleting user: %O', error);
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const finalError = new HttpError(404, 'Not Found', 'User not found', {
+          cause: error,
+        });
+        return next(finalError);
+      }
+
+      const finalError = new HttpError(
+        500,
+        'Internal Server Error',
+        'Failed to delete user',
+        {
+          cause: error,
+        },
+      );
+      return next(finalError);
+    }
+  }
+}
---
+++ src/users/users.routes.ts
@@ -0,0 +1,69 @@
+import { env } from '../config/env.ts';
+import debug from 'debug';
+import type { UsersController } from './users.controller.ts';
+import { Router } from 'express';
+import { validateBody, validateId } from '../middleware/validations.ts';
+import {
+  RegisterUserDTOSchema,
+  UserCredentialsDTOSchema,
+  UpdateUserDTOSchema,
+} from './user.schemas.zod.ts';
+import { AuthInterceptor } from '../middleware/auth.interceptor.ts';
+
+const log = debug(`${env.PROJECT_NAME}:router:users`);
+log('Loading users router...');
+
+export class UsersRouter {
+  private controller: UsersController;
+  private router: Router;
+  private authInterceptor: AuthInterceptor;
+  constructor(controller: UsersController, authInterceptor: AuthInterceptor) {
+    log('Initializing users router...');
+    this.controller = controller;
+    this.router = Router();
+    this.authInterceptor = authInterceptor;
+
+    // Define routes and bind them to controller methods
+    // For example:
+    this.router.post(
+      '/register',
+      validateBody(RegisterUserDTOSchema),
+      this.controller.register.bind(this.controller),
+    );
+    this.router.post(
+      '/login',
+      validateBody(UserCredentialsDTOSchema),
+      this.controller.login.bind(this.controller),
+    );
+    this.router.get(
+      '/',
+      this.authInterceptor.authenticate.bind(this.authInterceptor),
+      this.controller.getAllUsers.bind(this.controller),
+    );
+    this.router.get(
+      '/:id',
+      validateId(),
+      this.authInterceptor.authenticate.bind(this.authInterceptor),
+      this.controller.getUserById.bind(this.controller),
+    );
+    this.router.patch(
+      '/:id',
+      validateId(),
+      validateBody(UpdateUserDTOSchema),
+      this.authInterceptor.authenticate.bind(this.authInterceptor),
+      this.authInterceptor.isOwnerOrAdmin.bind(this.authInterceptor),
+      this.controller.updateUser.bind(this.controller),
+    );
+    this.router.delete(
+      '/:id',
+      validateId(),
+      this.authInterceptor.authenticate.bind(this.authInterceptor),
+      this.authInterceptor.isOwnerOrAdmin.bind(this.authInterceptor),
+      this.controller.deleteUser.bind(this.controller),
+    );
+  }
+
+  get router1() {
+    return this.router;
+  }
+}
